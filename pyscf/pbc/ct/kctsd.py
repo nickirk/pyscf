2618874af79a1803f0e6763b76585271c077d034
keliao <ke.liao.whu@gmail.com>
2022-12-01 18:29:11 -0800

    add get_mp2_amps

--- pyscf/pbc/ct/kctsd.py
+++ pyscf/pbc/ct/kctsd.py
@@ -15,13 +15,17 @@
 # 
 # Author: Ke Liao <ke.liao.whu@gmail.com>
 
-import numpy
+import numpy as np
 
 from pyscf import lib
+from pyscf.lib import logger
 from pyscf.ct import ctsd
 from pyscf.pbc import scf
 from pyscf.pbc.lib import kpts_helper
-from pyscf.pbc.cc.kccsd_rhf import RCCSD
+from pyscf.pbc.cc.kccsd_rhf import _get_epq
+from pyscf.pbc.mp.kmp2 import (get_frozen_mask, get_nocc, get_nmo,
+                               padded_mo_coeff, padding_k_idx)  # noqa
+
 
 kernel = pyscf.ct.ctsd.kernel
 
@@ -40,8 +44,61 @@ class RCTSD(ctsd.CTSD):
         return len(self.kpts)
     
     def init_amps(self):
+        
         if self._amps_algo == "mp2":
-            return self.get_mp2_amps()
+            return self.get_mp2_amps
+        else:
+            raise NotImplementedError
     
     def get_mp2_amps(self):
+        time0 = logger.process_clock(), logger.perf_counter()
+        # TODO: adapt kccsd_rhf._ERI for kctsd
+        t_nmo = self.t_nmo
+        e_nmo = self.e_nmo
+        nkpts = self.nkpts
+        t1 = np.zeros((nkpts, e_nmo, t_nmo), dtype=eris.fock.dtype)
+        t2 = np.empty((nkpts, nkpts, nkpts, e_nmo, e_nmo, t_nmo, t_nmo), dtype=eris.fock.dtype)
+        mo_e_p = [eris.mo_energy[k][:t_nmo] for k in range(nkpts)]
+        mo_e_x = [eris.mo_energy[k][t_nmo:] for k in range(nkpts)]
+
+        # Get location of padded elements in occupied and virtual space
+        nonzero_opadding, nonzero_vpadding = padding_k_idx(self, kind="split")
+
+        kconserv = self.khelper.kconserv
+        touched = np.zeros((nkpts, nkpts, nkpts), dtype=bool)
+        for kp, kq, kx in kpts_helper.loop_kkk(nkpts):
+            if touched[kp, kq, kx]:
+                continue
+
+            ky = kconserv[kp, kx, kq]
+            # For discussion of LARGE_DENOM, see t1new update above
+            e_xp = _get_epq([0, t_nmo, kp, mo_e_p, nonzero_opadding],
+                           [t_nmo, e_nmo, kx, mo_e_x, nonzero_vpadding],
+                           fac=[1.0,-1.0])
+
+            e_yq = _get_epq([0, t_nmo, kq, mo_e_p, nonzero_opadding],
+                           [t_nmo, e_nmo, ky, mo_e_x, nonzero_vpadding],
+                           fac=[1.0,-1.0])
+
+
+            e_pqxy = e_xp[:, None, :, None] + e_yq[:, None, :]
+
+            eris_pqxy = eris.oovv[kp, kq, kx]
+            eris_pqyx = eris.oovv[kp, kq, ky]
+            t2[kp, kq, kx] = eris_pqxy.conj() / e_pqxy
+
+            if kx != ky:
+                e_pqyx = e_pqxy.transpose(0, 1, 3, 2)
+                t2[kp, kq, ky] = eris_pqyx.conj() / e_pqyx
+
+            touched[kp, kq, kx] = touched[kp, kq, ky] = True
+
+        logger.timer(self, 'init mp2', *time0)
+        return t1, t2
+
+    get_mp2_amps = get_mp2_amps
+
+    def commute(self):
+        """commutation operator. Needs to take the k-conservation into account.
+        """
         pass
